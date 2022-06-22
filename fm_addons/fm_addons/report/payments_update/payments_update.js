571b60ec8a0af3d79a208f25b5486cd100cf5a81
Mohammad Ali <swe.mirza.ali@gmail.com>
2022-06-22 22:50:22 +0000

    payment update report

---
+++ fm_addons/fm_addons/report/payments_update/payments_update.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Mohammad Ali and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Payments Update"] = {
+	"filters": [
+
+	]
+};
