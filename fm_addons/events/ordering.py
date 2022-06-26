7f0fd7b968128b2cdabaaab6799d311ee1137c76
Mohammad Ali <swe.mirza.ali@gmail.com>
2022-06-26 06:04:51 +0500

    added create mr button and function

--- fm_addons/events/ordering.py
+++ fm_addons/events/ordering.py
@@ -8,13 +8,17 @@ from frappe.utils import nowdate
 
 @frappe.whitelist()
 def create_mr(item_code, qty):
-	mr = frappe.new_doc('Material Request')
-	mr.company = "Faizy.com"
-	mr.schedule_date = nowdate()
-	mr.transaction_date = nowdate()
-	mr.append('items',{
-		"item_code":item_code
-		"qty": qty
-		})
-	mr.save(ignore_permissions=True)
-	mr.submit()
+	try:
+		mr = frappe.new_doc('Material Request')
+		mr.company = "Faizy.com"
+		mr.schedule_date = nowdate()
+		mr.transaction_date = nowdate()
+		mr.append('items',{
+			"item_code":item_code
+			"qty": qty
+			})
+		mr.save(ignore_permissions=True)
+		mr.submit()
+		return mr.name
+	except Exception as ex:
+		frappe.log_error(frappe.get_traceback())
